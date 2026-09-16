1ed27f42220814c06ebddde4ed119cb7bb16bf10
HeT Developer <dev@het.invalid>
2026-09-16 14:41:37 +0800

    feat: add test mod

---
+++ include/mymod.hpp
@@ -0,0 +1,21 @@
+// Conan::ImportStart
+#pragma once
+#include <cstdint>
+// Conan::ImportEnd
+
+/**
+ * @brief [en] 111
+ * @brief [zh] 111
+ * @since 1.0
+ * @exporter
+ */
+void mymod_init(void);
+
+
+/**
+ * @brief [en] 111
+ * @brief [zh] 111
+ * @since 1.0
+ * @exporter
+ */
+int mymod_add(int a, int b);
---
+++ src/mymod.cpp
@@ -0,0 +1,23 @@
+// Conan::ImportStart
+#include <mymod.hpp>
+// Conan::ImportEnd
+
+/**
+ * @brief [en] 111 — implementation
+ * @brief [zh] 111 — 实现
+ * @since 1.0
+ */
+void mymod_init(void) {
+    // TODO: implement
+}
+
+/**
+ * @brief [en] 111 — implementation
+ * @brief [zh] 111 — 实现
+ * @since 1.0
+ * @exporter
+ */
+int mymod_add(int a, int b) {
+    // TODO: implement
+    return a + b;
+}
